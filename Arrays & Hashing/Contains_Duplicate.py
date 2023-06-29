afbb7614ec17a401f3e38bdaa6bc9ec1e0899698
Rathan <rathangarudas@gmail.com>
2023-06-29 14:00:09 -0500

    'Initial'

---
+++ Arrays & Hashing/Contains_Duplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        freq = {}
+        for i in nums:
+            if i in freq:
+                freq[i] += 1
+                return True
+            freq[i] = 1
+        return False
